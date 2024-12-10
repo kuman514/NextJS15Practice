4bcb62fb0ebe22dff4535ed6b17166b175cf2b9b
kumankoishi.py <7iggif@naver.com>
2024-12-10 16:38:43 +0900

    draft: apply next image

--- foodies-starting-project/components/main-header.js
+++ foodies-starting-project/components/main-header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 import LogoPng from '@/assets/logo.png';
 
@@ -8,7 +9,7 @@ export default function MainHeader() {
   return (
     <header className={classes.header}>
       <Link className={classes.logo} href="/">
-        <img src={LogoPng.src} alt="A plate with food on it" />
+        <Image src={LogoPng} alt="A plate with food on it" priority />
         NextLevel Food
       </Link>
 

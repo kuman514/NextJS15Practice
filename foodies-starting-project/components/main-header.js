8e5b2f863c93012366f05036749c0d2fbe44ca0f
kumankoishi.py <7iggif@naver.com>
2024-12-10 16:53:31 +0900

    refactor: move main header background to main header component

--- foodies-starting-project/components/main-header.js
+++ foodies-starting-project/components/main-header.js
@@ -2,27 +2,31 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import LogoPng from '@/assets/logo.png';
+import MainHeaderBackground from '@/components/main-header-background';
 
 import classes from './main-header.module.css';
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image src={LogoPng} alt="A plate with food on it" priority />
-        NextLevel Food
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={LogoPng} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }

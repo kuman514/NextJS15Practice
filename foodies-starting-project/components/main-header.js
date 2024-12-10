d8360d07c7ea9fa8dacdc718143c67174e300a05
kumankoishi.py <7iggif@naver.com>
2024-12-10 15:42:18 +0900

    draft: add & apply main header css module for styling

--- foodies-starting-project/components/main-header.js
+++ foodies-starting-project/components/main-header.js
@@ -2,15 +2,17 @@ import Link from 'next/link';
 
 import LogoPng from '@/assets/logo.png';
 
+import classes from './main-header.module.css';
+
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
+    <header className={classes.header}>
+      <Link className={classes.logo} href="/">
         <img src={LogoPng.src} alt="A plate with food on it" />
         NextLevel Food
       </Link>
 
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>

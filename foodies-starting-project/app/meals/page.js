b83225d23650dca7ec3009c1697f68312696257e
kumankoishi.py <7iggif@naver.com>
2024-12-13 01:44:21 +0900

    draft: add & apply meals-grid & meals-item to meals page

--- foodies-starting-project/app/meals/page.js
+++ foodies-starting-project/app/meals/page.js
@@ -1,20 +1,30 @@
 import Link from 'next/link';
 
+import classes from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+
 export default function MealsPage() {
   return (
-    <main>
-      <h1>Meals</h1>
-      <p>
-        <Link href="/meals/koishi-pasta">Koishi Pasta</Link>
-      </p>
-      <p>
-        <Link href="/meals/yasuo-pizza">Yasuo Pizza</Link>
-      </p>
-      <p>
-        <Link href="/meals/hoshino-whale-chocolate">
-          Hoshino Whale Chocolate
-        </Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        {/**
+         * @todo
+         * Browse meals data from database.
+         */}
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }

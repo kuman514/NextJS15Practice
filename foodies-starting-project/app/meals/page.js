7c88dffc50ebba568f5e4bd49b5c9815df13b472
kuman514 <hadjadj0@gmail.com>
2024-12-13 16:39:21 +0900

    fix: use suspense with async component instead of loading page to show loading with header

--- foodies-starting-project/app/meals/page.js
+++ foodies-starting-project/app/meals/page.js
@@ -3,11 +3,15 @@ import Link from 'next/link';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
+import { Suspense } from 'react';
 import classes from './page.module.css';
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
 
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -23,7 +27,11 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );

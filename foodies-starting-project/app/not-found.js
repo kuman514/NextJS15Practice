835c12ace6cdc7998151134ff5113013e52332a6
kuman514 <hadjadj0@gmail.com>
2024-12-13 17:00:28 +0900

    draft: add custom root not-found page

---
+++ foodies-starting-project/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <main className="not-found">
+      <h1>404 Not Found</h1>
+      <p>Could not find the requested page or resource.</p>
+    </main>
+  );
+}

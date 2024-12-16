03e09873e88b9a360a5d3b83c8d2f2277ce8289c
kuman514 <hadjadj0@gmail.com>
2024-12-16 16:47:08 +0900

    improvement: hide image input & add image picking button to image picker

--- foodies-starting-project/components/meals/image-picker.js
+++ foodies-starting-project/components/meals/image-picker.js
@@ -1,16 +1,38 @@
+'use client';
+
+import { useRef } from 'react';
+
 import classes from './image-picker.module.css';
 
 export default function ImagePicker({ label, name }) {
+  const imageInputRef = useRef(null);
+
+  function handleOnClick() {
+    imageInputRef.current?.click();
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
         <input
+          ref={imageInputRef}
+          className={classes.input}
           type="file"
           id={name}
           accept="image/png, image/jpeg"
           name={name}
         />
+        {/**
+         * Set button's type to "button" to prevent submitting form.
+         */}
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handleOnClick}
+        >
+          Pick an image
+        </button>
       </div>
     </div>
   );

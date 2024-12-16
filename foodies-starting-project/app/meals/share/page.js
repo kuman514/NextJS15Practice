d53619d07f319a339300fabe270ff37fe77cf4f0
kuman514 <hadjadj0@gmail.com>
2024-12-16 15:26:20 +0900

    draft: add & apply image-picker markups

--- foodies-starting-project/app/meals/share/page.js
+++ foodies-starting-project/app/meals/share/page.js
@@ -1,3 +1,4 @@
+import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 
 export default function MealsSharePage() {
@@ -38,7 +39,7 @@ export default function MealsSharePage() {
               required
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker label="Images" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>

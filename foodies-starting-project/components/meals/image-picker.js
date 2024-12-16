dfe887c87e120e54c86d73ff4d147df96f3e00ca
kuman514 <hadjadj0@gmail.com>
2024-12-16 18:05:51 +0900

    improvement: add picking image using file-input onchange & use-state

--- foodies-starting-project/components/meals/image-picker.js
+++ foodies-starting-project/components/meals/image-picker.js
@@ -1,20 +1,49 @@
 'use client';
 
-import { useRef } from 'react';
+import Image from 'next/image';
+import { useRef, useState } from 'react';
 
 import classes from './image-picker.module.css';
 
 export default function ImagePicker({ label, name }) {
   const imageInputRef = useRef(null);
 
+  const [pickedImage, setPickedImage] = useState(null);
+
   function handleOnClick() {
     imageInputRef.current?.click();
+  }
+
+  function handleOnChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
   }
 
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {pickedImage ? (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user"
+              fill
+            />
+          ) : (
+            <p>No image picked yet.</p>
+          )}
+        </div>
         <input
           ref={imageInputRef}
           className={classes.input}
@@ -22,6 +51,7 @@ export default function ImagePicker({ label, name }) {
           id={name}
           accept="image/png, image/jpeg"
           name={name}
+          onChange={handleOnChange}
         />
         {/**
          * Set button's type to "button" to prevent submitting form.

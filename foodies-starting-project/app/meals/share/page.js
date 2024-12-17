70d8be218de357577528a1ad10ff1891d9b1ddcf
kuman514 <hadjadj0@gmail.com>
2024-12-17 23:53:37 +0900

    improvement: add dynamic submit button rendering using form status

--- foodies-starting-project/app/meals/share/page.js
+++ foodies-starting-project/app/meals/share/page.js
@@ -1,6 +1,7 @@
 import ImagePicker from '@/components/meals/image-picker';
 import { shareMealAction } from '@/lib/action';
 
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 import classes from './page.module.css';
 
 export default function MealsSharePage() {
@@ -43,7 +44,7 @@ export default function MealsSharePage() {
           </p>
           <ImagePicker label="Images" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>

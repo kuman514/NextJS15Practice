9128a292331e7fe5c4a144d70bcad095fedaa736
kuman514 <hadjadj0@gmail.com>
2025-01-14 22:47:34 +0900

    fix: redirect to training if auth verified

--- auth-practice/app/page.js
+++ auth-practice/app/page.js
@@ -1,6 +1,15 @@
+import { redirect } from 'next/navigation';
+
 import AuthForm from '@/components/auth-form';
+import { verifyAuth } from '@/lib/auth';
 
 export default async function Home({ searchParams }) {
+  const result = await verifyAuth();
+
+  if (result.user) {
+    return redirect('/training');
+  }
+
   const formMode = searchParams.mode || 'login';
   return <AuthForm mode={formMode} />;
 }
